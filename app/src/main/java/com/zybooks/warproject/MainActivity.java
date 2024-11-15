e794689f8be18f600bd6e8f6683e4a1caabfdbda
Masontherock33 <mason01nass@gmail.com>
2024-11-15 16:26:54 -0500

    Update MainActivity.java

--- app/src/main/java/com/zybooks/warproject/MainActivity.java
+++ app/src/main/java/com/zybooks/warproject/MainActivity.java
@@ -3,6 +3,8 @@ package com.zybooks.warproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,6 +14,7 @@ import android.animation.ObjectAnimator;
 import android.animation.AnimatorSet;
 import android.animation.Animator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -246,7 +249,7 @@ public class MainActivity extends AppCompatActivity {
         dealBTN.setEnabled(false);
 
         // Trigger sword animation when war is declared
-        nimatorSet swordAnimationSet = createSwordAnimation();
+        AnimatorSet swordAnimationSet = createSwordAnimation();
         swordAnimationSet.start();
 
         handler.postDelayed(new Runnable() {
